d665404b535a587fc7579acc64caebbc589b58af
naveenmj7760 <naveenmj7760@LAPTOP-HBROTHS7>
2022-05-14 18:41:47 +0530

    1st commite

---
+++ Learn Java/src/myfirstjavaprogram/HellowWorld.java
@@ -0,0 +1,11 @@
+package myfirstjavaprogram;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Heloo World ");
+		System.out.print("This is 1st java code");
+	}
+
+}
